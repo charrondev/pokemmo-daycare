cb1e0c55426a5e426f9deb843ab48a19668d1a89
Adam Charron <adam@charrondev.com>
2020-02-25 21:47:24 -0500

    Style add menu

--- src/layout/AddMenu.tsx
+++ src/layout/AddMenu.tsx
@@ -3,14 +3,34 @@
  * @license MIT
  */
 
-import React, { useState } from "react";
-import { Menu, MenuButton, MenuList, MenuItem } from "@reach/menu-button";
+import { css } from "@emotion/core";
 import IconAdd from "@pokemmo/icons/IconAdd.svg";
 import { PokemonForm } from "@pokemmo/pokemon/PokemonForm";
+import {
+    borderRadius,
+    fontSizeLarge,
+    makeSingleBorder,
+} from "@pokemmo/styles/variables";
+import {
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    MenuPopover,
+} from "@reach/menu-button";
+import React, { useState } from "react";
 
 interface IProps {
     className?: string;
 }
+
+const menuItemStyles = css({
+    padding: "9px 18px",
+    "&:hover, &:active, &:focus": {
+        background: "#E7EAFF",
+        color: "inherit",
+    },
+});
 
 export function AddMenu(props: IProps) {
     const [showPokemonModal, setShowPokemonModal] = useState(false);
@@ -21,14 +41,54 @@ export function AddMenu(props: IProps) {
                 <MenuButton className={props.className}>
                     <IconAdd />
                 </MenuButton>
-                <MenuList>
-                    <MenuItem onSelect={() => alert("Add project")}>
-                        Add Project
-                    </MenuItem>
-                    <MenuItem onSelect={() => setShowPokemonModal(true)}>
-                        Add Pokemon
-                    </MenuItem>
-                </MenuList>
+                <MenuPopover
+                    position={(targetRect, ownRect) => {
+                        return {
+                            left: targetRect!.right + 9,
+                            top: targetRect?.top,
+                            width: ownRect?.width,
+                            minWidth: 160,
+                        };
+                    }}
+                    // style={{ left: "red" }}
+                    // css={{ left: "100% !important", top: 0 }}
+                >
+                    <div
+                        css={{
+                            position: "fixed",
+                            top: 0,
+                            bottom: 0,
+                            left: 66,
+                            right: 0,
+                            background: "rgba(0, 0, 0, 0.2)",
+                        }}
+                    ></div>
+                    <MenuItems
+                        css={{
+                            position: "relative",
+                            zIndex: 1,
+                            padding: "9px 0",
+                            border: makeSingleBorder(1),
+                            borderColor: "rgba(0, 0, 0, 0.1)",
+                            borderRadius: borderRadius,
+                            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                            fontSize: fontSizeLarge,
+                        }}
+                    >
+                        <MenuItem
+                            onSelect={() => alert("Add project")}
+                            css={menuItemStyles}
+                        >
+                            Add Project
+                        </MenuItem>
+                        <MenuItem
+                            onSelect={() => setShowPokemonModal(true)}
+                            css={menuItemStyles}
+                        >
+                            Add Pokemon
+                        </MenuItem>
+                    </MenuItems>
+                </MenuPopover>
             </Menu>
             {showPokemonModal && (
                 <PokemonForm
